73041a86e016c44e1f036de7965c884d58c7f56e
Guillermito <a@a.com.ar>
2024-12-09 19:23:37 -0300

    Pagina Terrrazza al 9/12

---
+++ terrrazza.js
@@ -0,0 +1,73 @@
+function cargarDrinks(){
+    const texto=document.getElementById("texto");
+    texto.style.color="rgba(0,0,0,0.0)";
+    const shows=document.getElementById("shows");
+    shows.style.display="block";
+    shows.style.backgroundImage='url(drinks.jpg)';
+    shows.style.backgroundSize='100% 100%';
+    /*height: 500px;  You must set a specified height */
+    shows.style.backgroundPosition='center';
+    /*shows.backgroundPosition='center';*/
+    shows.style.backgroundRepeat='no-repeat'; /* Do not repeat the image */
+}
+
+function cargarDance(){
+    const texto=document.getElementById("texto");
+    texto.style.color="rgba(0,0,0,0.0)";
+    const shows=document.getElementById("shows");
+    shows.style.backgroundImage='url(dance.jpg)';
+    shows.style.backgroundSize='100% 100%';
+    shows.style.backgroundRepeat='no-repeat';
+}
+
+function cargarFriends(){   
+    const texto=document.getElementById("texto");
+    texto.style.color="rgba(0,0,0,0.0)";
+    const shows=document.getElementById("shows");
+    shows.style.backgroundImage='url(friends.jpg)';
+    shows.style.backgroundSize='100% 100%';
+    shows.style.backgroundRepeat='no-repeat';
+}
+
+function cargarMusic(){
+    const texto=document.getElementById("texto");
+    texto.style.color="rgba(0,0,0,0.0)";
+    const shows=document.getElementById("shows");
+    shows.style.backgroundImage='url(music.jpg)';
+    shows.style.backgroundSize='100% 100%';
+    shows.style.backgroundRepeat='no-repeat';
+}
+
+function cargarNight(){
+    const texto=document.getElementById("texto");
+    texto.style.color="rgba(0,0,0,0.0)";
+    const shows=document.getElementById("shows");
+    shows.style.backgroundImage='url(nights.jpg)';
+    shows.style.backgroundSize='100% 100%';
+    shows.style.backgroundRepeat='no-repeat';
+}
+
+function cargarFashion(){    
+    const texto=document.getElementById("texto");
+    texto.style.color="rgba(0,0,0,0.0)";
+    const shows=document.getElementById("shows");
+    shows.style.backgroundImage='url(fashion.jpg)';
+    shows.style.backgroundSize='100% 100%';
+    shows.style.backgroundRepeat='no-repeat';
+}
+
+function cargarGirls(){
+    const texto=document.getElementById("texto");
+    texto.style.color="rgba(0,0,0,0.0)";
+    const shows=document.getElementById("shows");
+    shows.style.backgroundImage='url(girls.jpg)';
+    shows.style.backgroundSize='100% 100%';
+    shows.style.backgroundRepeat='no-repeat';
+}
+
+function cargarShows(){
+    const texto=document.getElementById("texto");
+    texto.style.color="black";
+    const shows=document.getElementById("shows");
+    shows.style.background='rgb(160, 160, 93)';
+}
